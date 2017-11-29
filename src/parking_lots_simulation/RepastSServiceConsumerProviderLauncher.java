ff9f2b66c82eff5e062d40093c74ab3f9bb576d6
Nuno Ramos <nunor07.unfc@gmail.com>
2017-11-29 10:53:56 +0000

    Adding name to main platform

--- src/parking_lots_simulation/RepastSServiceConsumerProviderLauncher.java
+++ src/parking_lots_simulation/RepastSServiceConsumerProviderLauncher.java
@@ -3,6 +3,8 @@ package parking_lots_simulation;
 import jade.core.Profile;
 import jade.core.ProfileImpl;
 import jade.wrapper.StaleProxyException;
+import repast.simphony.context.Context;
+import repast.simphony.context.space.graph.NetworkBuilder;
 import sajas.core.Runtime;
 import sajas.sim.repasts.RepastSLauncher;
 import sajas.wrapper.ContainerController;
@@ -16,32 +18,46 @@ public class RepastSServiceConsumerProviderLauncher extends RepastSLauncher{
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Parking Lots Simulation";
 	}
 
 	@Override
 	protected void launchJADE() {
+		
+		System.out.println("Boasssssss");
 
 		Runtime rt = Runtime.instance();
 		Profile p1 = new ProfileImpl();
+		System.out.println(rt.toString());
 		mainContainer = rt.createMainContainer(p1);
+		
+		//mainContainer = rt.createAgentContainer(p1);
 
 		launchAgents();
+	}
+	
+	@Override
+	public Context build(Context<Object> context) {
+		
+		NetworkBuilder<Object> netBuilder = new NetworkBuilder<Object>("Service Consumer/Provider network", context, true);
+		netBuilder.buildNetwork();
+		
+		return super.build(context);
 	}
 
 	public void launchAgents() {
 
 		try {
-
+			System.out.println("Boasssssss1111");
 			// create explorer driver agents
 			for (int i = 0; i < N_EXPLORER_DRIVERS; i++) {
 				ExplorerDriverAgent ed = new ExplorerDriverAgent();
 				mainContainer.acceptNewAgent("ExplorerDriver" + i, ed).start();
+				System.out.println("aaaaaaaaaaaaaaaa"); 
 			}
 
 			// create guided driver agents
-			for (int i = 0; i < N_EXPLORER_DRIVERS; i++) {
+			for (int i = 0; i < N_GUIDED_DRIVERS; i++) {
 				ExplorerDriverAgent gd = new ExplorerDriverAgent();
 				mainContainer.acceptNewAgent("GuidedDriver" + i, gd).start();
 			}
