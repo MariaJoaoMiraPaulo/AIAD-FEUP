4f11757dee5ef7c47ea9c24910ea32190c1ccfea
Bernardo Belchior <bernardo.belchior1@gmail.com>
2017-12-03 18:53:10 +0000

    Changes distance comparator to use Double.compare

--- src/parking_lots_simulation/RepastSServiceConsumerProviderLauncher.java
+++ src/parking_lots_simulation/RepastSServiceConsumerProviderLauncher.java
@@ -26,12 +26,13 @@ import sajas.wrapper.ContainerController;
 
 public class RepastSServiceConsumerProviderLauncher extends RepastSLauncher{
 
-	private static final int N_STATIC_PARKING_FACILITY = 20;
-	private static final int N_DYNAMIC_PARKING_FACILITY = 0;
-	private static int N_EXPLORER_DRIVERS = 20;
-	private static int N_GUIDED_DRIVERS = 0;
-	private static int GRID_WIDTH_SIZE = 50;
-	private static int GRID_HEIGHT_SIZE = 50;
+
+	private static final int N_STATIC_PARKING_FACILITY = 5;
+	private static final int N_DYNAMIC_PARKING_FACILITY = 5;
+	private static final int N_EXPLORER_DRIVERS = 10;
+	private static final int N_GUIDED_DRIVERS = 10;
+	private static final int GRID_WIDTH_SIZE = 50;
+	private static final int GRID_HEIGHT_SIZE = 50;
 	private static ArrayList<ParkingFacilityAgent> parkingFacilities = new ArrayList<>();
 
 	private ContainerController mainContainer;
--- src/parking_lots_simulation/behaviours/GuidedDriverBehaviour.java
+++ src/parking_lots_simulation/behaviours/GuidedDriverBehaviour.java
@@ -1,9 +1,11 @@
 package parking_lots_simulation.behaviours;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import parking_lots_simulation.DriverAgent;
 import parking_lots_simulation.ParkingFacilityAgent;
+import parking_lots_simulation.comparators.SortByDistance;
 import repast.simphony.space.grid.Grid;
 import sajas.core.behaviours.Behaviour;
 
@@ -13,10 +15,21 @@ public class GuidedDriverBehaviour extends Behaviour {
 	 * 
 	 */
 	private static final long serialVersionUID = -766675944062469676L;
-
-	public GuidedDriverBehaviour(DriverAgent guidedDriver, Grid<Object> mainGrid,
-			ArrayList<ParkingFacilityAgent> parkingFacilities) {
-		// TODO Auto-generated constructor stub
+	private Grid<Object> mainGrid;
+	private DriverAgent driverAgent;
+	private ArrayList<ParkingFacilityAgent> parkingFacilities;
+	private boolean done = false;
+	
+	public GuidedDriverBehaviour(DriverAgent guidedDriver, Grid<Object> mainGrid, ArrayList<ParkingFacilityAgent> parkingFacilities) {
+		this.driverAgent = guidedDriver;
+		this.mainGrid = mainGrid;
+		this.parkingFacilities = parkingFacilities;
+		
+		Collections.sort(this.parkingFacilities, new SortByDistance(this.mainGrid, this.mainGrid.getLocation(this.driverAgent)));
+		
+		for(int i = 0; i < this.parkingFacilities.size(); i++) {
+			double distance = mainGrid.getDistance(this.mainGrid.getLocation(this.driverAgent), this.mainGrid.getLocation(this.parkingFacilities.get(i)));
+		}
 	}
 
 	@Override
@@ -27,8 +40,7 @@ public class GuidedDriverBehaviour extends Behaviour {
 
 	@Override
 	public boolean done() {
-		// TODO Auto-generated method stub
-		return true;
+		return done;
 	}
 
 }
