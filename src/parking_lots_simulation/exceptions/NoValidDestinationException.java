3d6e6eff83bc177dba04b1facc9d9d3824d99457
Nuno Ramos <nunor07.unfc@gmail.com>
2017-12-09 23:58:12 +0000

    Removing warnings

--- src/parking_lots_simulation/exceptions/NoValidDestinationException.java
+++ src/parking_lots_simulation/exceptions/NoValidDestinationException.java
@@ -2,4 +2,9 @@ package parking_lots_simulation.exceptions;
 
 public class NoValidDestinationException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4915503009235517470L;
+
 }
